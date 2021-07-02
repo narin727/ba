2546e897608442556cc0f661418c371767f71833
narin727 <rhkrskfls@naver.com>
2021-07-03 02:59:42 +0900

    first commit

--- lionproject/settings.py
+++ lionproject/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    'DJANGO_SECRET_KEY','django-insecure-4@dm@+r+iwrf_n_nvfs8nw@dpm8)_+l97nwbr34sg@ww_kstsd')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-4@dm@+r+iwrf_n_nvfs8nw@dpm8)_+l97nwbr34sg@ww_kstsd')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
@@ -140,8 +139,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 #AWS
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_ACCESS_KEY_ID = 'AKIATT27MD47HV3BDGCX'
+AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'likelion-django-lesson727'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_REGION_NAME = 'ap-northeast-2'
